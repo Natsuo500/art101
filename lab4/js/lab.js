f8f976ba0c16af21a716f4344f8cc7ccde79a471
Natsuo500 <160647952+Natsuo500@users.noreply.github.com>
2024-10-17 23:21:38 -0700

    Update lab.js

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,7 +1,6 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
-
+// index.js - Rock-Paper-Scissors
+// Author: Bryan Galvan
+// Date:10/17/2024
 // Constants
 
 // Functions
@@ -19,3 +18,20 @@ function main() {
 
 // let's get this party started
 main();
+
+
+//Set scene
+//Give three options: Rock, Paper, or Scissors
+//Choose computer move (not visible to player)
+//Prompt user to choose an option (rock, paper, scissors)
+    //Paper > Rock > Scissors > Paper
+    //if player picks rock and Computer picks scissors player wins
+    //if player picks paper and computer picks rock player wins
+    //if player picks scissors and computer picks paper player wins
+    //if computer picks rock and player picks scissors computer wins
+    //if computer picks paper and player picks rock computer wins
+    //if computer picks scissors and player picks paper computer wins
+  //Test for win or draws conditions 
+    //base on outcome will show who won
+//Restart
+  //start again 

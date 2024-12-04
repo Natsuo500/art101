b8a72ff8f3e5ff8c306c55deea7788570c83dc8c
Natsuo500 <160647952+Natsuo500@users.noreply.github.com>
2024-12-04 00:52:33 -0800

    lab 16 fixed

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -3,23 +3,25 @@ let endpoint = "https://api.nasa.gov/planetary/apod";
 
 // Configure the AJAX request
 let ajaxConfig = {
-  url: endpoint, // API URL
-  type: "GET", // HTTP method
-  dataType: "json",
-  data: {
-    api_key:"kZyabAD0PcwPuAkgAOBB2yZPiu2QwfizgL1UMrpp", // API token
-    count: 1,
-  },
-  success: function(data) { // Success handler
-    let record = data[0]; 
-    $("#output").append("<h2>" + record.title);
-    $("#output").append(`<img src='${record.url}' />`);
-    $("#output").append("<p>" + record.explanation);
-  }, 
-  error: function(xhr, status, error) { // Error handler
-    console.error(error); 
-  } 
-};
-
-// Send the AJAX request
-$.ajax(ajaxConfig);
+    url: endpoint, // API URL
+    type: "GET", // HTTP method
+    dataType: "json",
+    data: {
+      api_key:"kZyabAD0PcwPuAkgAOBB2yZPiu2QwfizgL1UMrpp",
+      count: 1,
+      // API token
+    },
+  success: function(data) { 
+      let record = data[0];// Success handler
+      console.log(data); 
+        $("#output").append("<h2>" + record.title);
+        $("#output").append(`<img src= '${record.url}' />`);
+        $("#output").append("<p>" + record.explanation);
+    }, 
+    error: function(xhr, status, error) { // Error handler
+      console.error(error); 
+    } 
+  };
+  
+  // Send the AJAX request
+  $.ajax(ajaxConfig);
